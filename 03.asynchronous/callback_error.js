0d350ec8976b9f26403d6c8272bdddc01e0b4c5c
Ryzhon <pekozono@email.com>
2024-02-15 13:47:51 +0900

    db接続時にエラーが起こると処理を中断するように修正

--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -2,8 +2,7 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:", (err) => {
   if (err) {
-    console.error("データベース接続エラー:", err.message);
-    return;
+    throw new Error("データベース接続エラー:", "err.message");
   }
   console.log("メモリ内のSQLiteデータベースに接続しました。");
 });
