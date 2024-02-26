b78b18645e0a6d7bf1dd43362cde5c9245cd473c
Ryzhon <ryuta.hokazono@gmail.com>
2024-02-26 13:17:39 +0900

    ネストの修正 理由：正常に実行されないことがあるため

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -12,47 +12,55 @@ connectToDatabase()
       db,
       "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
     )
-      .then(() =>
-        databaseRun(
-          db,
-          "INSERT INTO books (title) VALUES (?)",
-          "非同期処理入門",
-        ),
-      )
-      .catch((err) => {
-        console.error("初回レコード挿入エラー:", err.message);
+      .then(() => {
+        console.log("テーブルを作成しました。");
+        return db;
       })
+      .catch((err) => {
+        console.error("テーブル作成エラー:", err.message);
+      });
+  })
+  .then((db) => {
+    return databaseRun(
+      db,
+      "INSERT INTO books (title) VALUES (?)",
+      "非同期処理入門",
+    )
       .then((result) => {
         console.log(`レコードを挿入しました。ID: ${result.lastID}`);
-        return databaseGet(
-          db,
-          "SELECT * FROM books WHERE id = ?",
-          result.lastID,
-        );
+        return { db, result };
       })
       .catch((err) => {
         console.error("レコード挿入エラー:", err.message);
-        return Promise.resolve();
-      })
+      });
+  })
+  .then(({ db, result }) => {
+    return databaseGet(db, "SELECT * FROM books WHERE id = ?", result.lastID)
       .then((row) => {
         console.log(`取得したレコード: ID: ${row.id}, Title: ${row.title}`);
-        return databaseRun(db, "DROP TABLE books");
+        return db;
       })
+      .catch((err) => {
+        console.error("レコード挿入エラー:", err.message);
+      });
+  })
+  .then((db) => {
+    return databaseRun(db, "DROP TABLE books")
       .then(() => {
         console.log("テーブルを削除しました。");
+        return db;
       })
       .catch((err) => {
         console.error("テーブル削除エラー:", err.message);
+      });
+  })
+  .then((db) => {
+    return closeDatabase(db)
+      .then(() => {
+        console.log("データベース接続を閉じました。");
       })
-      .finally(() => {
-        closeDatabase(db)
-          .then(() => {
-            console.log("データベース接続を閉じました。");
-          })
-
-          .catch((err) => {
-            console.error("データベース接続終了エラー:", err.message);
-          });
+      .catch((err) => {
+        console.error("データベース接続終了エラー:", err.message);
       });
   })
   .catch((err) => {
--- 03.asynchronous/promise_error.js
+++ 03.asynchronous/promise_error.js
@@ -12,51 +12,72 @@ connectToDatabase()
       db,
       "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
     )
-      .then(() =>
-        databaseRun(
-          db,
-          "INSERT INTO books (title) VALUES (?)",
-          "非同期処理入門",
-        ),
-      )
-      .catch((err) => {
-        console.error("初回レコード挿入エラー:", err.message);
+      .then(() => {
+        console.log("テーブルを作成しました。");
+        return db;
       })
+      .catch((err) => {
+        console.error("テーブル作成エラー:", err.message);
+      });
+  })
+  .then((db) => {
+    return databaseRun(
+      db,
+      "INSERT INTO books (title) VALUES (?)",
+      "非同期処理入門",
+    )
       .then((result) => {
         console.log(`レコードを挿入しました。ID: ${result.lastID}`);
-        return databaseRun(
-          db,
-          "INSERT INTO books (title) VALUES (?)",
-          "非同期処理入門",
-        );
+        return db;
+      })
+      .catch((err) => {
+        console.error("レコード挿入エラー:", err.message);
+      });
+  })
+  .then((db) => {
+    return databaseRun(
+      db,
+      "INSERT INTO books (title) VALUES (?)",
+      "非同期処理入門",
+    )
+      .then((result) => {
+        console.log(`レコードを挿入しました。ID: ${result.lastID}`);
+        return db;
       })
       .catch((err) => {
         console.error("二回目レコード挿入エラー:", err.message);
-        return Promise.resolve();
-      })
-      .then(() => databaseGet(db, "SELECT * FROM books WHERE id = ?", 999))
+        return db;
+      });
+  })
+  .then((db) => {
+    return databaseGet(db, "SELECT * FROM books WHERE id = ?", 999)
       .then((row) => {
         if (!row) {
           console.log("指定されたIDのレコードは存在しません。");
+          return db;
         } else {
           console.log(`取得したレコード: ID: ${row.id}, Title: ${row.title}`);
         }
       })
       .catch((err) => {
         console.error("レコード取得エラー:", err.message);
-      })
+      });
+  })
+  .then((db) => {
+    return databaseRun(db, "DROP TABLE books")
       .then(() => {
-        databaseRun(db, "DROP TABLE books");
+        console.log("テーブルを削除しました。");
+        return db;
       })
-      .catch((err) => console.error("テーブル削除エラー:", err.message))
-      .finally(() => {
-        closeDatabase(db)
-          .then(() => {
-            console.log("データベース接続を閉じました。");
-          })
-          .catch((err) => {
-            console.error("データベース接続終了エラー:", err.message);
-          });
+      .catch((err) => console.error("テーブル削除エラー:", err.message));
+  })
+  .then((db) => {
+    return closeDatabase(db)
+      .then(() => {
+        console.log("データベース接続を閉じました。");
+      })
+      .catch((err) => {
+        console.error("データベース接続終了エラー:", err.message);
       });
   })
   .catch((err) => {
