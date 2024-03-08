260e1b1b6afdcae00c9a0368d285c22434877fa1
Ryzhon <ryuta.hokazono@gmail.com>
2024-03-08 11:56:03 +0900

    fix: プロミスのログ出力

--- 03.asynchronous/promise_error.js
+++ 03.asynchronous/promise_error.js
@@ -1,51 +1,49 @@
-import { openDatabaseConnection, closeDatabaseConnection } from "./db_utils.js";
 import {
-  createBooksTable,
-  insertBook,
-  getBook,
-  dropBooksTable,
-} from "./promise_utils.js";
+  openDatabaseConnection,
+  closeDatabaseConnection,
+  runDatabaseQuery,
+  getDatabaseData,
+} from "./db_utils.js";
 
 let dbInstance = null;
+
 openDatabaseConnection(":memory:")
-  .catch((err) => {
-    console.error("データベース接続エラー:", err.message);
-    throw err;
-  })
   .then((db) => {
     dbInstance = db;
-    console.log("SQLiteデータベースに接続しました。");
-    return createBooksTable(db);
+    console.log("メモリ内のSQLiteデータベースに接続しました。");
   })
-  .catch((err) => console.error("テーブル作成エラー:", err.message))
-  .then(() => {
-    console.log("テーブルを作成しました。");
-    return insertBook(dbInstance);
-  })
-  .catch((err) => console.error("レコード挿入エラー:", err.message))
-  .then((result) => {
-    console.log(`レコードを挿入しました。ID: ${result.lastID}`);
-    return insertBook(dbInstance);
-  })
+  .then(() =>
+    runDatabaseQuery(
+      dbInstance,
+      "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+    ),
+  )
+  .then(() => console.log("テーブルを作成しました。"))
+  .then(() =>
+    runDatabaseQuery(
+      dbInstance,
+      "INSERT INTO books (title) VALUES (?)",
+      "非同期処理入門",
+    ),
+  )
+  .then((result) => console.log(`レコードを挿入しました。ID: ${result.lastID}`))
+  .then(() =>
+    runDatabaseQuery(
+      dbInstance,
+      "INSERT INTO books (title) VALUES (?)",
+      "非同期処理入門",
+    ),
+  )
   .catch((err) => console.error("二回目レコード挿入エラー:", err.message))
-  .then(() => {
-    const fakeResult = { lastID: 999 };
-    return getBook(dbInstance, fakeResult);
-  })
+  .then(() =>
+    getDatabaseData(
+      dbInstance,
+      "SELECT * FROM non_existing_table WHERE id = ?",
+      1,
+    ),
+  )
   .catch((err) => console.error("レコード取得エラー:", err.message))
-  .then((row) => {
-    if (!row) {
-      console.log("指定されたIDのレコードは存在しません。");
-    } else {
-      console.log(`取得したレコード: ID: ${row.id}, Title: ${row.title}`);
-    }
-    return dropBooksTable(dbInstance);
-  })
-  .catch((err) => console.error("テーブル削除エラー:", err.message))
-  .then(() => {
-    console.log("テーブルを削除しました。");
-    return closeDatabaseConnection(dbInstance);
-  })
-  .catch((err) => console.error("データベース接続終了エラー:", err.message))
-  .then(() => console.log("データベース接続を閉じました。"))
-  .catch((err) => console.error(`エラー: ${err.message}`));
+  .then(() => runDatabaseQuery(dbInstance, "DROP TABLE books"))
+  .then(() => console.log("テーブルを削除しました。"))
+  .then(() => closeDatabaseConnection(dbInstance))
+  .then(() => console.log("データベース接続を閉じました。"));
