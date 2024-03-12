0631c5a833d75f036a3f2444f62f4ebfbbc9a5b5
Ryzhon <ryuta.hokazono@gmail.com>
2024-03-12 10:27:46 +0900

    fix: asyncのエラーハンドリングをなくす。async_errorのエラーが発生されるところのみをtry, catchで囲む

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -7,33 +7,30 @@ import {
 
 let db;
 
-try {
-  db = await openDatabaseConnection(":memory:");
-  console.log("SQLiteデータベースに接続しました。");
+db = await openDatabaseConnection(":memory:");
+console.log("SQLiteデータベースに接続しました。");
 
-  await runDatabaseQuery(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
-  );
-  console.log("テーブルを作成しました。");
+await runDatabaseQuery(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+);
+console.log("テーブルを作成しました。");
 
-  const result = await runDatabaseQuery(
-    db,
-    "INSERT INTO books (title) VALUES (?)",
-    "非同期処理入門",
-  );
-  console.log(`レコードを挿入しました。ID: ${result.lastID}`);
+const result = await runDatabaseQuery(
+  db,
+  "INSERT INTO books (title) VALUES (?)",
+  "非同期処理入門",
+);
+console.log(`レコードを挿入しました。ID: ${result.lastID}`);
 
-  const row = await getDatabaseData(
-    db,
-    "SELECT * FROM books WHERE id = ?",
-    result.lastID,
-  );
-  console.log(`取得したレコード: ID: ${row.id}, Title: ${row.title}`);
+const row = await getDatabaseData(
+  db,
+  "SELECT * FROM books WHERE id = ?",
+  result.lastID,
+);
+console.log(`取得したレコード: ID: ${row.id}, Title: ${row.title}`);
 
-  await (db, "DROP TABLE books");
-  console.log("テーブルを削除しました。");
-} finally {
-  await closeDatabaseConnection(db);
-  console.log("データベース接続を閉じました。");
-}
+await (db, "DROP TABLE books");
+console.log("テーブルを削除しました。");
+await closeDatabaseConnection(db);
+console.log("データベース接続を閉じました。");
--- 03.asynchronous/async_error.js
+++ 03.asynchronous/async_error.js
@@ -23,25 +23,32 @@ try {
     "非同期処理入門",
   );
   console.log(`レコードを挿入しました。ID: ${result.lastID}`);
-
-  await runDatabaseQuery(
-    db,
-    "INSERT INTO books (title) VALUES (?)",
-    "非同期処理入門",
-  );
-  console.log(`二回目のレコードを挿入しました。ID: ${result.lastID}`);
+  try {
+    await runDatabaseQuery(
+      db,
+      "INSERT INTO books (title) VALUES (?)",
+      "非同期処理入門",
+    );
+    console.log(`二回目のレコードを挿入しました。ID: ${result.lastID}`);
+  } catch (err) {
+    console.error("二回目レコード取得エラー:", err.message);
+    throw err;
+  }
+  try {
+    await getDatabaseData(
+      db,
+      "SELECT * FROM non_existing_table WHERE id = ?",
+      1,
+    );
+  } catch (err) {
+    console.error("レコード取得エラー:", err.message);
+    throw err;
+  }
 } catch (err) {
-  console.error("二回目レコード取得エラー:", err.message);
-}
-try {
-  await getDatabaseData(db, "SELECT * FROM non_existing_table WHERE id = ?", 1);
-} catch (err) {
-  console.error("レコード取得エラー:", err.message);
-}
-try {
+  console.error("エラー:", err.message);
+} finally {
   await runDatabaseQuery(db, "DROP TABLE books");
   console.log("テーブルを削除しました。");
-} finally {
   await closeDatabaseConnection(db);
   console.log("データベース接続を閉じました。");
 }
